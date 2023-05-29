b94ffbc259e12594886d668ba521519e61e793f5
Ario Dwiponco <rio.dwiponco@gmail.com>
2023-05-29 15:32:17 +0700

    [FIX] Multiple invoice error

--- vz_purchase_downpayment/models/purchase.py
+++ vz_purchase_downpayment/models/purchase.py
@@ -119,122 +119,123 @@ class PurchaseOrder(models.Model):
                                      self.order_line.filtered(lambda l: not l.is_downpayment)]
         return super(PurchaseOrder, self).copy_data(default)
 
-    def action_create_invoice(self):
-        """Create the invoice associated to the PO.
-        """
-        if (self.purchase_active_id.advance_payment_method != 'delivered') and ('purchase_orders' in self.env.context):
-            po_line_values = self.env.context.get('po_line_values')
-        else:
-            if 'po_line_values' not in self.env.context:
-                raise UserError(_('Down payment context is missing...'))
-
-        purchase_line_id = {}
-        precision = self.env['decimal.precision'].precision_get('Product Unit of Measure')
-        # 1) Prepare invoice vals and clean-up the section lines
-        seq = ''
-        invoice_vals_list = []
-        for order in self:
-            if order.invoice_status != 'to invoice':
-                continue
-            order = order.with_company(order.company_id)
-            pending_section = None
-            # Invoice values.
-
-            invoice_vals = order._prepare_invoice()
-            # Invoice line values (keep only necessary sections).
-
-            for line in order.order_line:
-                if order.purchase_active_id.advance_payment_method != 'delivered':
-                    if line.display_type == 'line_section':
-                        pending_section = line
-                        continue
-                else:
-                    if not line.is_downpayment and line.display_type == 'line_section':
-                        pending_section = line
-                        continue
-
-                if not float_is_zero(line.qty_to_invoice, precision_digits=precision):
-                    if order.purchase_active_id.advance_payment_method == 'delivered':
-                        if line.is_downpayment and (
-                                self.purchase_active_id.advance_payment_method == 'delivered' and not self.purchase_active_id.deduct_down_payments):
-                            continue
-                        if pending_section:
-                            invoice_vals['invoice_line_ids'].append(
-                                (0, 0, pending_section._prepare_account_move_line()))
-                            pending_section = None
-                        invoice_vals['invoice_line_ids'].append((0, 0, line._prepare_account_move_line()))
-                    else:
-                        if pending_section:
-                            get_invoice_line = pending_section.prepare_downpayment_account_move_line(po_line_values)
-                            invoice_vals['invoice_line_ids'].append((0, 0, get_invoice_line))
-                            pending_section = None
-                        get_invoice_line = line.prepare_downpayment_account_move_line(po_line_values)
-                        invoice_vals['invoice_line_ids'].append((0, 0, get_invoice_line))
-                        purchase_line_id = get_invoice_line['purchase_line_id']
-            invoice_vals_list.append(invoice_vals)
-
-        if not invoice_vals_list:
-            raise UserError(_('There is no invoiceable line. If a product has a control policy based on received '
-                              'quantity, please make sure that a quantity has been received.'))
-
-        # 2) group by (company_id, partner_id, currency_id) for batch creation
-
-        new_invoice_vals_list = []
-        for grouping_keys, invoices in groupby(invoice_vals_list, key=lambda x: (
-                x.get('company_id'), x.get('partner_id'), x.get('currency_id'))):
-            origins = set()
-            payment_refs = set()
-            refs = set()
-            ref_invoice_vals = None
-            for invoice_vals in invoices:
-                if not ref_invoice_vals:
-                    ref_invoice_vals = invoice_vals
-                else:
-                    ref_invoice_vals['invoice_line_ids'] += invoice_vals['invoice_line_ids']
-                origins.add(invoice_vals['invoice_origin'])
-                payment_refs.add(invoice_vals['payment_reference'])
-                refs.add(invoice_vals['ref'])
-            ref_invoice_vals.update({
-                'ref': ', '.join(refs)[:2000],
-                'invoice_origin': ', '.join(origins),
-                'payment_reference': len(payment_refs) == 1 and payment_refs.pop() or False,
-            })
-
-            new_invoice_vals_list.append(ref_invoice_vals)
-        invoice_vals_list = new_invoice_vals_list
-
-        if self.purchase_active_id.advance_payment_method != 'delivered':
-            if po_line_values:
-                invoice_line_ids = [(0, 0, {'display_type': False,
-                                            'sequence': int(po_line_values['sequence']),
-                                            'name': po_line_values['label'],
-                                            'product_id': int(po_line_values['product_id']),
-                                            'product_uom_id': 1,
-                                            'quantity': 1.0,
-                                            'price_unit': float(po_line_values['price_unit']),
-                                            'tax_ids': po_line_values['taxes_id'],
-                                            'purchase_line_id': purchase_line_id  # Bill count
-                                            })]
-                invoice_vals_list[0].update(invoice_line_ids=invoice_line_ids)
-                if self.partner_ref:
-                    reference = _("Downpayment: %s [%s]" % (self.ref_count, invoice_vals_list[0]['ref']))
-                    invoice_vals_list[0]['ref'] = reference
-                    invoice_vals_list[0]['payment_reference'] = reference
-                    self.ref_count = self.ref_count + 1
-
-        # 3) Create bill.
-        moves = self.env['account.move']
-        AccountMove = self.env['account.move'].with_context(default_move_type='in_invoice')
-        for vals in invoice_vals_list:
-            moves |= AccountMove.with_company(vals['company_id']).create(vals)
-
-        moves.filtered(
-            lambda m: m.currency_id.round(m.amount_total) < 0).action_switch_invoice_into_refund_credit_note()
-
-        # return self.action_view_invoice(moves)
-        if self._context.get('open_invoices', False):
-            return self.action_view_invoice(moves)
-        return {'type': 'ir.actions.act_window_close'}
+    # def action_create_invoice(self):
+    #     """Create the invoice associated to the PO.
+    #     """
+    #     for record in self:
+    #         if (record.purchase_active_id.advance_payment_method != 'delivered') and ('purchase_orders' in record.env.context):
+    #             po_line_values = record.env.context.get('po_line_values')
+    #         else:
+    #             if 'po_line_values' not in self.env.context:
+    #                 raise UserError(_('Down payment context is missing...'))
+    #
+    #         purchase_line_id = {}
+    #         precision = self.env['decimal.precision'].precision_get('Product Unit of Measure')
+    #         # 1) Prepare invoice vals and clean-up the section lines
+    #         seq = ''
+    #         invoice_vals_list = []
+    #         for order in self:
+    #             if order.invoice_status != 'to invoice':
+    #                 continue
+    #             order = order.with_company(order.company_id)
+    #             pending_section = None
+    #             # Invoice values.
+    #
+    #             invoice_vals = order._prepare_invoice()
+    #             # Invoice line values (keep only necessary sections).
+    #
+    #             for line in order.order_line:
+    #                 if order.purchase_active_id.advance_payment_method != 'delivered':
+    #                     if line.display_type == 'line_section':
+    #                         pending_section = line
+    #                         continue
+    #                 else:
+    #                     if not line.is_downpayment and line.display_type == 'line_section':
+    #                         pending_section = line
+    #                         continue
+    #
+    #                 if not float_is_zero(line.qty_to_invoice, precision_digits=precision):
+    #                     if order.purchase_active_id.advance_payment_method == 'delivered':
+    #                         if line.is_downpayment and (
+    #                                 record.purchase_active_id.advance_payment_method == 'delivered' and not record.purchase_active_id.deduct_down_payments):
+    #                             continue
+    #                         if pending_section:
+    #                             invoice_vals['invoice_line_ids'].append(
+    #                                 (0, 0, pending_section._prepare_account_move_line()))
+    #                             pending_section = None
+    #                         invoice_vals['invoice_line_ids'].append((0, 0, line._prepare_account_move_line()))
+    #                     else:
+    #                         if pending_section:
+    #                             get_invoice_line = pending_section.prepare_downpayment_account_move_line(po_line_values)
+    #                             invoice_vals['invoice_line_ids'].append((0, 0, get_invoice_line))
+    #                             pending_section = None
+    #                         get_invoice_line = line.prepare_downpayment_account_move_line(po_line_values)
+    #                         invoice_vals['invoice_line_ids'].append((0, 0, get_invoice_line))
+    #                         purchase_line_id = get_invoice_line['purchase_line_id']
+    #             invoice_vals_list.append(invoice_vals)
+    #
+    #         if not invoice_vals_list:
+    #             raise UserError(_('There is no invoiceable line. If a product has a control policy based on received '
+    #                               'quantity, please make sure that a quantity has been received.'))
+    #
+    #         # 2) group by (company_id, partner_id, currency_id) for batch creation
+    #
+    #         new_invoice_vals_list = []
+    #         for grouping_keys, invoices in groupby(invoice_vals_list, key=lambda x: (
+    #                 x.get('company_id'), x.get('partner_id'), x.get('currency_id'))):
+    #             origins = set()
+    #             payment_refs = set()
+    #             refs = set()
+    #             ref_invoice_vals = None
+    #             for invoice_vals in invoices:
+    #                 if not ref_invoice_vals:
+    #                     ref_invoice_vals = invoice_vals
+    #                 else:
+    #                     ref_invoice_vals['invoice_line_ids'] += invoice_vals['invoice_line_ids']
+    #                 origins.add(invoice_vals['invoice_origin'])
+    #                 payment_refs.add(invoice_vals['payment_reference'])
+    #                 refs.add(invoice_vals['ref'])
+    #             ref_invoice_vals.update({
+    #                 'ref': ', '.join(refs)[:2000],
+    #                 'invoice_origin': ', '.join(origins),
+    #                 'payment_reference': len(payment_refs) == 1 and payment_refs.pop() or False,
+    #             })
+    #
+    #             new_invoice_vals_list.append(ref_invoice_vals)
+    #         invoice_vals_list = new_invoice_vals_list
+    #
+    #         if record.purchase_active_id.advance_payment_method != 'delivered':
+    #             if po_line_values:
+    #                 invoice_line_ids = [(0, 0, {'display_type': False,
+    #                                             'sequence': int(po_line_values['sequence']),
+    #                                             'name': po_line_values['label'],
+    #                                             'product_id': int(po_line_values['product_id']),
+    #                                             'product_uom_id': 1,
+    #                                             'quantity': 1.0,
+    #                                             'price_unit': float(po_line_values['price_unit']),
+    #                                             'tax_ids': po_line_values['taxes_id'],
+    #                                             'purchase_line_id': purchase_line_id  # Bill count
+    #                                             })]
+    #                 invoice_vals_list[0].update(invoice_line_ids=invoice_line_ids)
+    #                 if record.partner_ref:
+    #                     reference = _("Downpayment: %s [%s]" % (record.ref_count, invoice_vals_list[0]['ref']))
+    #                     invoice_vals_list[0]['ref'] = reference
+    #                     invoice_vals_list[0]['payment_reference'] = reference
+    #                     record.ref_count = record.ref_count + 1
+    #
+    #         # 3) Create bill.
+    #         moves = self.env['account.move']
+    #         AccountMove = self.env['account.move'].with_context(default_move_type='in_invoice')
+    #         for vals in invoice_vals_list:
+    #             moves |= AccountMove.with_company(vals['company_id']).create(vals)
+    #
+    #         moves.filtered(
+    #             lambda m: m.currency_id.round(m.amount_total) < 0).action_switch_invoice_into_refund_credit_note()
+    #
+    #         # return self.action_view_invoice(moves)
+    #         if self._context.get('open_invoices', False):
+    #             return self.action_view_invoice(moves)
+    #         return {'type': 'ir.actions.act_window_close'}
 
 
 class PurchaseOrderLine(models.Model):
