43c4893592b2d403b148dd467c42251d89f4fae3
inicoding <irfannurrahmat@outlook.com>
2023-06-19 09:09:12 +0700

    [FIX] fix error maufacture

--- solinda_manufacture/__manifest__.py
+++ solinda_manufacture/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'mrp', 'purchase_stock','sol_purchase','solinda_mrp', 'sale', 'stock', 'quality_mrp'],
+    'depends': ['base', 'mrp', 'purchase_stock','sol_purchase','solinda_mrp', 'sale', 'stock', 'quality_mrp', 'sol_stock'],
 
     # always loaded
     'data': [
