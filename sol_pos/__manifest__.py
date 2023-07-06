b6d7bf5f25e1ea0792fa932c7774fe895b2d2eab
Ario Dwiponco <rio.dwiponco@gmail.com>
2023-07-06 17:00:07 +0700

    [FIX] Manifest

--- sol_pos/__manifest__.py
+++ sol_pos/__manifest__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '15.0.1.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'point_of_sale', 'product', 'report_xlsx'], 
@@ -33,7 +33,7 @@
     ],
     'assets': {
             'web.assets_backend': [
-                'remove_decimal_zero_trailing/static/src/js/field_utils.js',
+                'sol_pos/static/src/js/field_utils.js',
                 ]
         },
 }
