6763268fcd67fc40984efd90b0b0956270181c3b
Ario Dwiponco <rio.dwiponco@gmail.com>
2023-07-07 16:08:39 +0700

    [WIP] Analytic account PoS

--- sol_pos/models/__init__.py
+++ sol_pos/models/__init__.py
@@ -2,6 +2,8 @@
 
 from . import stock_needs
 from . import pos_config
+from . import pos_order
+from . import pos_session
 from . import res_users
 from . import stock_comment
 from . import ir_qweb_fields
--- sol_pos/models/pos_config.py
+++ sol_pos/models/pos_config.py
@@ -1,7 +1,9 @@
-from odoo import models, fields, api
+from odoo import models, fields
+
 
 class PosConfig(models.Model):
-  _inherit = 'pos.config'
+    _inherit = 'pos.config'
 
-  address = fields.Char(string='Address')
-  supervisor = fields.Char(string='Supervisor')
+    address = fields.Char(string='Address')
+    supervisor = fields.Char(string='Supervisor')
+    analytic_account_id = fields.Many2one(comodel_name="account.analytic.account", string="Analytic Account")
