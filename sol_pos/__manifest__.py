30032d4556f8638e8fc1416f70566a6eada67259
Ario Dwiponco <rio.dwiponco@gmail.com>
2023-07-10 10:24:51 +0700

    [UPD] Custom receipt for BB

--- sol_pos/__manifest__.py
+++ sol_pos/__manifest__.py
@@ -17,18 +17,21 @@
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '15.0.1.0.5',
+    'version': '15.0.1.0.7',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'point_of_sale', 'product', 'report_xlsx'],
+    'depends': ['base', 'point_of_sale', 'pos_sale', 'product', 'report_xlsx'],
 
     # always loaded
     'data': [
+        'data/receipt.xml',
         'security/ir.model.access.csv',
         'security/security.xml',
         'views/stock_need_views.xml',
         'views/stock_comment_views.xml',
         'views/pos_config_views.xml',
+        'views/point_of_sale_views.xml',
+        'views/pos_receipt_views.xml',
         'views/res_users_view.xml',
         'views/sequence_data.xml',
         'report/action_report.xml',
@@ -38,7 +41,9 @@
             'sol_pos/static/src/js/field_utils.js',
         ],
         'point_of_sale.assets': [
-            'sol_pos/static/src/js/pos_screem.js',
+            'sol_pos/static/src/js/product_screen.js',
+            'sol_pos/static/src/js/order_receipt.js',
+            'sol_pos/static/src/js/models.js',
         ],
         'web.assets_qweb': [
             'sol_pos/static/src/xml/pos.xml',
