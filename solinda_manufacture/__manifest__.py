86e4712e7f786d3c955b464d97263f804c4b4ab9
Ario Dwiponco <rio.dwiponco@gmail.com>
2023-08-02 10:27:00 +0700

    [UPD] Margin PPS

--- solinda_manufacture/__manifest__.py
+++ solinda_manufacture/__manifest__.py
@@ -1,27 +1,25 @@
 # -*- coding: utf-8 -*-
 {
     'name': "solinda_manufacture",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+    'summary': """Custom solinda MRP procedure for BambooBlonde""",
 
     'description': """
-        Long description of module's purpose
+        Production Sample
+        Pre-production sample
     """,
 
     'author': "Luthfi A.Nizar - 08998046065",
-    'website': "http://www.yourcompany.com",
+    'website': "http://www.solinda.co.id",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    "version": "15.1.0.0.1",
+    "version": "15.1.0.0.2",
     'license': "LGPL-3",
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'mrp', 'purchase_stock','sol_purchase','solinda_mrp', 'sale', 'stock', 'quality_mrp',],
+    'depends': ['base', 'mrp', 'purchase_stock', 'sol_purchase','solinda_mrp', 'sale', 'stock', 'quality_mrp'],
 
     # always loaded
     'data': [
