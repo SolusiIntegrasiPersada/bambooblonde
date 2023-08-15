7dfe8ccb25d1d02a4777d198a6e52fc8826bb822
inicoding <irfannurrahmat@outlook.com>
2023-08-15 12:53:53 +0700

    [UPD] qty on hand per location

--- sol_bb_product/models/product.py
+++ sol_bb_product/models/product.py
@@ -18,8 +18,35 @@ class ProductProduct(models.Model):
                                                           relation='product_variant_combination',
                                                           domain=[('attribute_line_id.value_count', '>', 0)],
                                                           string="Variant Values", ondelete='restrict')
+    internal_location = fields.One2many('stock.quantity', 'product_id', compute='get_product_qty')
 
+    def get_product_qty(self):
+        location_list = []
+        product_list = []
+        obj_location = self.env['stock.location'].search([('usage', '=', 'internal')])
+        for i in obj_location:
+            location_list.append(i.id)
+        obj_quant = self.env['stock.quant'].search([('product_id', '=', self.id),
+                                                    ('location_id', 'in', location_list)])
+        for obj in obj_quant:
+            move_line = {'product_id': obj.product_id.id,
+                         'stock_location': obj.location_id.id,
+                         'qty_on_hand': obj.available_quantity,
+                         }
+            product_list.append(move_line)
+        for i in product_list:
+            if i['qty_on_hand'] > 0:
+                self.internal_location |= self.env['stock.quantity'].create(i)
 
+class InternalLocation(models.Model):
+    _name = 'stock.quantity'
+
+    stock_location = fields.Many2one('stock.location', string='Location Name')
+    qty_on_hand = fields.Float('On Hand')
+    forecast = fields.Float('Forecast')
+    incoming_qty = fields.Float('Incoming Quantity')
+    outgoing_qty = fields.Float('Outgoing Quantity')
+    product_id = fields.Many2one('product.product', string='Product')
 class ProductCollections(models.Model):
     _name = "product.collections"
     _description = "Product Collections"
