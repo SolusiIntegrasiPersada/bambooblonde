efed9a65165d953c9d9499490361a962237ddb4d
inicoding <irfannurrahmat@outlook.com>
2023-09-27 14:46:20 +0700

    [FIX] report production

--- sol_purchase/report/production_po.py
+++ sol_purchase/report/production_po.py
@@ -3,6 +3,9 @@ from odoo import fields, api, models
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
+    def get_last_record_order(self):
+        for record in self:
+            return record.order_line.sorted(lambda l: l.id, reverse=False)[0]
     def consolidate_lines(self):
         consolidated_lines = {}
         column = []
@@ -21,10 +24,10 @@ class PurchaseOrder(models.Model):
         column_c = ['8','9','34','35','12-13','B','XS','S/M','16']
         column_d = ['10','11','36','37','13-14','C','XSS','M/L','17']
         column_e = ['12','38','39','14-15','D','S','L/XL','18']
-        column_f = ['14','40','41','15-16','M']
+        column_f = ['14','40','41','15-16','M','ALL']
         column_g = ['4','42','43','37-38','L']
         column_h = ['38-39','XL']
-        column_i = ['OS','ALL']
+        column_i = ['OS']
 
         total = 0
         type = []
@@ -57,26 +60,106 @@ class PurchaseOrder(models.Model):
             qtyy = item_data['qtyy']
             price = item_data['price']
             item_type = None
+            size_column_b = None
+            size_column_c = None
+            size_column_d = None
+            size_column_e = None
+            size_column_f = None
+            size_column_g = None
+            size_column_h = None
+            size_column_i = None
 
             # Determine item_type based on matching types
             if all(size in type_a for size in sizes):
                 item_type = 'A'
+                size_column_b = '6'
+                size_column_c = '8'
+                size_column_d = '10'
+                size_column_e = '12'
+                size_column_f = '14'
+                size_column_g = '4'
+                size_column_h = ''
+                size_column_i = ''
             elif all(size in type_b for size in sizes):
                 item_type = 'B'
+                size_column_b = '7'
+                size_column_c = '9'
+                size_column_d = '11'
+                size_column_e = ''
+                size_column_f = ''
+                size_column_g = ''
+                size_column_h = ''
+                size_column_i = ''
             elif all(size in type_c for size in sizes):
                 item_type = 'C'
+                size_column_b = '32'
+                size_column_c = '34'
+                size_column_d = '36'
+                size_column_e = '38'
+                size_column_f = '40'
+                size_column_g = '42'
+                size_column_h = ''
+                size_column_i = ''
             elif all(size in type_d for size in sizes):
                 item_type = 'D'
+                size_column_b = '33'
+                size_column_c = '35'
+                size_column_d = '37'
+                size_column_e = '39'
+                size_column_f = '41'
+                size_column_g = '43'
+                size_column_h = ''
+                size_column_i = ''
             elif all(size in type_e for size in sizes):
                 item_type = 'E'
+                size_column_b = '11-12'
+                size_column_c = '12-13'
+                size_column_d = '13-14'
+                size_column_e = '14-15'
+                size_column_f = '15-16'
+                size_column_g = '37-38'
+                size_column_h = '38-39'
+                size_column_i = ''
             elif all(size in type_f for size in sizes):
                 item_type = 'F'
+                size_column_b = 'A'
+                size_column_c = 'B'
+                size_column_d = 'C'
+                size_column_e = 'D'
+                size_column_f = ''
+                size_column_g = ''
+                size_column_h = ''
+                size_column_i = ''
             elif all(size in type_g for size in sizes):
                 item_type = 'G'
+                size_column_b = 'XXS'
+                size_column_c = 'XS'
+                size_column_d = 'XSS'
+                size_column_e = 'S'
+                size_column_f = 'M'
+                size_column_g = 'L'
+                size_column_h = 'XL'
+                size_column_i = 'OS'
             elif all(size in type_h for size in sizes):
                 item_type = 'H'
+                size_column_b = 'XS/S'
+                size_column_c = 'S/M'
+                size_column_d = 'M/L'
+                size_column_e = 'L/XL'
+                size_column_f = 'ALL'
+                size_column_g = ''
+                size_column_h = ''
+                size_column_i = ''
             elif all(size in type_i for size in sizes):
                 item_type = 'I'
+                size_column_b = '15'
+                size_column_c = '16'
+                size_column_d = '17'
+                size_column_e = '18'
+                size_column_f = '14'
+                size_column_g = ''
+                size_column_h = ''
+                size_column_i = ''
             else:
                 item_type = None
 
@@ -102,6 +185,14 @@ class PurchaseOrder(models.Model):
                 'qty': qtyy,
                 'price': price,
                 'item_type': item_type,
+                'size_column_b': size_column_b,
+                'size_column_c': size_column_c,
+                'size_column_d': size_column_d,
+                'size_column_e': size_column_e,
+                'size_column_f': size_column_f,
+                'size_column_g': size_column_g,
+                'size_column_h': size_column_h,
+                'size_column_i': size_column_i,
                 'amount_b': amounts['amount_b'],
                 'amount_c': amounts['amount_c'],
                 'amount_d': amounts['amount_d'],
