ea394f753f5a19fbb134a0e1a0492ce857779f35
firmanrizaldi <firman.yusuf@hashmicro.com>
2023-11-03 08:08:11 +0700

    [add] warung dan shift in name journal?

--- sol_pos/models/pos_session.py
+++ sol_pos/models/pos_session.py
@@ -1,4 +1,12 @@
-from odoo import models, fields
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from collections import defaultdict
+from datetime import timedelta
+
+from odoo import api, fields, models, _, Command
+from odoo.exceptions import AccessError, UserError, ValidationError
+from odoo.tools import float_is_zero, float_compare
 
 
 class PosSession(models.Model):
@@ -33,3 +41,54 @@ class PosSession(models.Model):
     #         'tax_tag_ids': [(6, 0, base_tag_ids)],
     #     }
     #     return self._credit_amounts(partial_vals, amount, amount_converted)
+    
+    def _create_combine_account_payment(self, payment_method, amounts, diff_amount):
+        outstanding_account = payment_method.outstanding_account_id or self.company_id.account_journal_payment_debit_account_id
+        destination_account = self._get_receivable_account(payment_method)
+
+        if float_compare(amounts['amount'], 0, precision_rounding=self.currency_id.rounding) < 0:
+            # revert the accounts because account.payment doesn't accept negative amount.
+            outstanding_account, destination_account = destination_account, outstanding_account
+            
+        shift = (' - %s') % (self.shift) if self.shift else  ""
+
+        account_payment = self.env['account.payment'].create({
+            'amount': abs(amounts['amount']),
+            'journal_id': payment_method.journal_id.id,
+            'force_outstanding_account_id': outstanding_account.id,
+            'destination_account_id':  destination_account.id,
+            'ref': _('Combine %s POS payments from %s (%s%s)') % (payment_method.name, self.name, self.config_id.name,shift),
+            'pos_payment_method_id': payment_method.id,
+            'pos_session_id': self.id,
+        })
+
+        diff_amount_compare_to_zero = self.currency_id.compare_amounts(diff_amount, 0)
+        if diff_amount_compare_to_zero != 0:
+            self._apply_diff_on_account_payment_move(account_payment, payment_method, diff_amount)
+
+        account_payment.action_post()
+        return account_payment.move_id.line_ids.filtered(lambda line: line.account_id == account_payment.destination_account_id)
+    
+    def _get_combine_statement_line_vals(self, statement, amount, payment_method):
+        shift = (' - %s') % (self.shift) if self.shift else  ""
+        return {
+            'date': fields.Date.context_today(self),
+            'amount': amount,
+            'payment_ref':  ('%s (%s%s)') % (self.name, self.config_id.name,shift),
+            'statement_id': statement.id,
+            'journal_id': statement.journal_id.id,
+            'counterpart_account_id': self._get_receivable_account(payment_method).id,
+        }
+
+    def _get_split_statement_line_vals(self, statement, amount, payment):
+        accounting_partner = self.env["res.partner"]._find_accounting_partner(payment.partner_id)
+        shift = (' - %s') % (self.shift) if self.shift else  ""
+        return {
+            'date': fields.Date.context_today(self, timestamp=payment.payment_date),
+            'amount': amount,
+            'payment_ref':  ('%s (%s%s)') % (self.name, self.config_id.name,shift),
+            'statement_id': statement.id,
+            'journal_id': statement.journal_id.id,
+            'counterpart_account_id': accounting_partner.property_account_receivable_id.id,
+            'partner_id': accounting_partner.id,
+        }
